f052e1eddb0ccb00999776190ffeb0592430255c
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2019-11-08 13:09:13 +0000

    Fix catalog options.

--- options/data/real/RealData_2015_Charm_MagUp_catalog.py
+++ options/data/real/RealData_2015_Charm_MagUp_catalog.py
@@ -1,4 +1,4 @@
 
 from Gaudi.Configuration import FileCatalog
 
-FileCatalog().Catalogs += [ 'xmlcatalog_file:$AGAMMAD0TOHHPI0ROOT/options/data/real/2015_Charm_MagUp_catalog.xml' ]
+FileCatalog().Catalogs += [ 'xmlcatalog_file:$AGAMMAD0TOHHPI0ROOT/options/data/real/RealData_2015_Charm_MagUp_catalog.xml' ]
--- options/data/real/RealData_2017_Charm_MagDown_catalog.py
+++ options/data/real/RealData_2017_Charm_MagDown_catalog.py
@@ -1,4 +1,4 @@
 
 from Gaudi.Configuration import FileCatalog
 
-FileCatalog().Catalogs += [ 'xmlcatalog_file:$AGAMMAD0TOHHPI0ROOT/options/data/real/2017_Charm_MagDown_catalog.xml' ]
+FileCatalog().Catalogs += [ 'xmlcatalog_file:$AGAMMAD0TOHHPI0ROOT/options/data/real/RealData_2017_Charm_MagDown_catalog.xml' ]
